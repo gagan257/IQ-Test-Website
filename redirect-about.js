8dabca001a96ade88d1e1a192ddf187c6f6b07d5
gagan257 <arorag2577@gmail.com>
2022-12-28 13:32:04 +0530

    redirect pages

---
+++ redirect-about.js
@@ -0,0 +1,6 @@
+(function redirect () {
+    setTimeout(myURL, 2000);
+})();
+function myURL(){
+    document.location.href = 'about.html';
+}
---
+++ redirect-submit.js
@@ -0,0 +1,6 @@
+(function redirect () {
+    setTimeout(myURL, 5000);
+})();
+function myURL(){
+    document.location.href = 'submit.html';
+}
